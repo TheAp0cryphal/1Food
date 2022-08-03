e49d78301e3c1b2fa9e05861473a1d4908860cd2
Ching Hang Lam <chinghang0504@gmail.com>
2022-08-03 08:44:40 -0700

    The logout is completed

--- app/src/main/java/com/project/onefood/MainMenu/UserProfileActivity.kt
+++ app/src/main/java/com/project/onefood/MainMenu/UserProfileActivity.kt
@@ -1,16 +1,52 @@
 package com.project.onefood.MainMenu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import com.google.firebase.auth.FirebaseAuth
+import com.project.onefood.Login.LoginActivity
 import com.project.onefood.R
+import com.project.onefood.databinding.ActivityUserProfileBinding
 import com.project.onefood.showtoast
 
 class UserProfileActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+
+    // UI
+    private lateinit var binding: ActivityUserProfileBinding
+
+    // Actions on create
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initActivity()
+
+        setListeners()
+    }
+
+    // Initialize the activity
+    private fun initActivity() {
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    // Set the listeners
+    private fun setListeners() {
+        binding.logoutButton.setOnClickListener {
+            clickLogoutButton()
+        }
+    }
+
+    // Click the logout button
+    private fun clickLogoutButton() {
+        FirebaseAuth.getInstance().signOut()
+
+        val intent: Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
 
     override fun onResume() {
         super.onResume()
@@ -27,12 +63,6 @@ class UserProfileActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_profile)
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
