f38425a993100e01752f5350d3cc7a73762c5433
sparsh <sparshsharma9990@gmail.com>
2022-08-02 22:30:10 -0700

    Minor Fixes

--- app/src/main/java/com/project/onefood/RestaurantPage/FragmentAdapters/MenuAdapter.kt
+++ app/src/main/java/com/project/onefood/RestaurantPage/FragmentAdapters/MenuAdapter.kt
@@ -15,31 +15,32 @@ class MenuAdapter(var context : Context) : BaseAdapter() {
     private val inflater: LayoutInflater
             = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    var foodItems = arrayOf("pizza", "pasta", "chicken noodles")
-
+    var foodnames = arrayOf("pizza", "pasta", "chicken noodles","yam fries")
+    var foodPrices = arrayOf("$19.99", "$15.99", "$17.99","$9.99")
+    var foodImages = arrayOf(R.drawable.fake_promo, R.drawable.fake_promo2, R.drawable.fake_promo3, R.drawable.fake_promo4)
     override fun getCount(): Int {
-        return 3
+        return 4
     }
 
     override fun getItem(p0: Int): Any {
-        return foodItems[p0]
+        return foodnames[p0]
     }
 
     override fun getItemId(p0: Int): Long {
-        return foodItems[p0].toLong()
+        return foodnames[p0].toLong()
     }
 
     override fun getView(position: Int, view : View?, parent : ViewGroup?): View {
         val rowView = inflater.inflate(R.layout.menu_row, parent, false)
 
         val foodImage = rowView.findViewById<ImageView>(R.id.food_img)
-        foodImage.setImageResource(R.drawable.fake_promo)
+        foodImage.setImageResource(foodImages[position])
 
         val foodName = rowView.findViewById<TextView>(R.id.food_name)
-        foodName.text = foodItems[position]
+        foodName.text = foodnames[position]
 
         val foodPrice =  rowView.findViewById<TextView>(R.id.food_price)
-        foodPrice.text = "$10.99"
+        foodPrice.text = foodPrices[position]
 
         return rowView
     }
